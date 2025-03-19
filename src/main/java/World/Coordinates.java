0ad4e07ff31c051a9a89fb33b2499be6adb2e69c
Vladimir Tolstov <tolstov@gmail.com>
2025-03-19 16:06:14 +0700

    add coordinates class

---
+++ src/main/java/World/Coordinates.java
@@ -0,0 +1,42 @@
+package World;
+
+import java.util.Objects;
+
+public class Coordinates {
+
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
