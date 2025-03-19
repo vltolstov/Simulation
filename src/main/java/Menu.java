2d8cbfcafbd49bdd4e59520e6c1b1a325bf8f21c
Vladimir Tolstov <tolstov@gmail.com>
2025-03-19 11:29:53 +0700

    add select state method

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,34 +1,33 @@
 public class Menu {
 
-    private final static String CMD_START = "старт";
     private final static String CMD_STOP = "стоп";
-    private final static String START_MESSAGE = "Для запуска введите - " + CMD_START;
+    private final static String CMD_PAUSE = "пауза";
     private final static String STOP_MESSAGE = "Для выхода из игры введите - " + CMD_STOP;
-    private final static String WRONG_COMMAND_MESSAGE = "Не верная команда. Попробуйте еще раз.";
-    private static boolean STATE_MENU = true;
+    private final static String PAUSE_MESSAGE = "Для паузы игры введите - " + CMD_PAUSE;
+    private final static String WRONG_COMMAND_MESSAGE = "Неверная команда. Попробуйте еще раз.";
 
     public Menu() {
         showMenuMessages();
     }
 
     private void showMenuMessages() {
-        Notifier.showMessage(START_MESSAGE);
         Notifier.showMessage(STOP_MESSAGE);
+        Notifier.showMessage(PAUSE_MESSAGE);
     }
 
-    public void setGameState() {
-        do{
-            String command = CommandReader.getUserInput();
+    public GameState selectGameState(GameState state) {
 
-            if (command.equals(CMD_START)){
-                STATE_MENU = false;
-            } else if (command.equals(CMD_STOP)){
-                STATE_MENU = false;
-                // стейт игры тоже false
-            } else {
-                Notifier.showMessage(WRONG_COMMAND_MESSAGE);
-            }
+        String command = CommandReader.getUserInput();
 
-        } while (STATE_MENU);
+        if(command.equals(CMD_STOP)) {
+            return state = GameState.STOP;
+        } else if (command.equals(CMD_PAUSE)) {
+            return state = GameState.PAUSE;
+        } else {
+            Notifier.showMessage(WRONG_COMMAND_MESSAGE);
+        }
+
+        return state;
     }
 }
+
