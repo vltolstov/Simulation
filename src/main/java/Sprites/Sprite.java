74dbd2ec7e61ebb9ac55e06d33dd84c821255c41
Vladimir Tolstov <tolstov@gmail.com>
2025-03-22 16:33:42 +0700

    add Sprite class

---
+++ src/main/java/Sprites/Sprite.java
@@ -0,0 +1,12 @@
+package Sprites;
+
+public class Sprite {
+
+    public String spriteCode;
+    private static final String EMPTY_SPRITE_CODE = "   ";
+
+    public static String getEmptySprite() {
+        return EMPTY_SPRITE_CODE;
+    }
+
+}
