f4a65286f6c50e3cd8e036ee919402a7045fa597
Vladimir Tolstov <tolstov@gmail.com>
2025-03-25 17:13:00 +0700

    add toString method

--- src/main/java/World/Coordinates.java
+++ src/main/java/World/Coordinates.java
@@ -54,4 +54,9 @@ public class Coordinates {
     public int hashCode() {
         return Objects.hash(x, y);
     }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
 }
